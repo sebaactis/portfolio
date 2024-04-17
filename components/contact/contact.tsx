793c6d56130ff6351c3b7f089031edeeb41fdcd6
Sebastian Actis <Sebastian.Actis@globalprocessing.com.ar>
2024-04-17 17:08:26 -0300

    contacto v2

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,10 +1,13 @@
 import { bebasNeue, manrope } from '@/utils/fonts'
 import { Github, Instagram, LinkedinIcon, Twitter } from 'lucide-react'
-import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from '@/components/ui/button'
 
 const Contact = () => {
     return (
-        <section className="flex justify-between md:ml-28 mt-16 mb-10">
+        <section className="flex md:ml-28 mt-16 mb-10">
             <article className="flex flex-col gap-6">
                 <h2 className={`${bebasNeue.className} md:text-7xl text-6xl`}> LETS CONNECT </h2>
                 <div className="flex flex-col gap-3">
@@ -30,15 +33,25 @@ const Contact = () => {
                 </div>
                 <div className="flex gap-5">
                     <LinkedinIcon className="text-indigo-500" />
-                    <Github className="text-indigo-500"/>
-                    <Twitter className="text-indigo-500"/>
-                    <Instagram className="text-indigo-500"/>
+                    <Github className="text-indigo-500" />
+                    <Twitter className="text-indigo-500" />
+                    <Instagram className="text-indigo-500" />
                 </div>
 
             </article>
-            <article>
-                Formulario TODO
-            </article>
+            <form className="md:ml-80 flex flex-col gap-5">
+                <Label htmlFor="name">Name</Label>
+                <Input className="md:w-[30rem]" type="text" id="name" />
+                <Label htmlFor="email">Email</Label>
+                <Input className="md:w-[30rem]" type="email" id="email" />
+                <Label htmlFor="subject">Subject</Label>
+                <Input className="md:w-[30rem]" type="text" id="subject" />
+                <Label htmlFor="message">Message</Label>
+                <Textarea placeholder="Type your message here." id="message" />
+                <Button className="rounded-3xl py-6 px-5 text-black bg-indigo-400 hover:bg-indigo-400 dark:bg-indigo-400 dark:hover:bg-indigo-400 font-bold hover:scale-[1.02] text-md transition duration-150">SUBMIT
+
+                </Button>
+            </form>
         </section>
 
     )
